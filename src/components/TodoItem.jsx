42a4f4194b1c93ded2371c593621b95bde8d8812
Fakhri <muhammadfakhri1234@gmail.com>
2024-06-07 22:53:18 +0700

    develop_delete_todo_feature

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-const TodoItem = ({ todo, toggleCompleted }) => {
+const TodoItem = ({ todo, toggleCompleted, deleteTodo }) => {
   const getTodoTitleStyle = () => {
-    if (todo.completed === true) {
-      return { textDecoration: "line-through" };
-    } else {
-      return { textDecoration: "none" };
-    }
+    return todo.completed
+      ? { textDecoration: "line-through" }
+      : { textDecoration: "none" };
   };
 
   return (
@@ -14,10 +12,13 @@ const TodoItem = ({ todo, toggleCompleted }) => {
       <input
         type="checkbox"
         style={styles.checkbox}
-        //  Memberikan id dari todo sebagai argument
         onChange={() => toggleCompleted(todo.id)}
+        checked={todo.completed} // memastikan checkbox mengikuti state completed
       />
       <p style={getTodoTitleStyle()}>{todo.title}</p>
+      <button style={styles.button} onClick={() => deleteTodo(todo.id)}>
+        x
+      </button>
     </div>
   );
 };
@@ -26,16 +27,25 @@ const styles = {
   todoItem: {
     border: "2px solid #f4f4f4",
     fontSize: "24px",
-    // Tambahkan styles di bawah ini
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "center",
+    padding: "0 20px",
+    marginBottom: "10px", // memberikan jarak antar item
   },
-  // Tambahkan styles di bawah ini
   checkbox: {
-    marginRight: "10px",
     height: "18px",
     width: "18px",
+  },
+  button: {
+    backgroundColor: "#BB0000",
+    color: "#fff",
+    height: "30px",
+    width: "30px",
+    borderRadius: "100%",
+    border: "none",
+    cursor: "pointer",
+    fontSize: "16px",
   },
 };
 
