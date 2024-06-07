fa116276f403aacb9aa8c1705ab011f8a14ec53f
Fakhri <muhammadfakhri1234@gmail.com>
2024-06-07 22:30:56 +0700

    Develop toggle todo feature

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-const Todos = ({ todos }) => {
+const Todos = ({ todos, toggleCompleted }) => {
   return (
-    <div>
+    <div style={styles.container}>
       {todos.map((todo) => {
-        // Berikan datanya ke TodoItem
-        return <TodoItem key={todo.id} todo={todo} />;
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            // Teruskan function toggleCompleted ke component TodoItem
+            toggleCompleted={toggleCompleted}
+          />
+        );
       })}
     </div>
   );
+};
+
+const styles = {
+  container: {
+    width: "40%",
+    margin: "0 auto",
+  },
 };
 
 export default Todos;
